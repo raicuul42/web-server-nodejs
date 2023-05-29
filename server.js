ee6b9511cc784761aeae5b20d585cdc120bd7ef1
Raisul Amin R <raicuul42@users.noreply.github.com>
2023-05-29 17:18:45 +0800

    remove gitignore files

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('Halo, kita akan belajar membuat server');
+/* eslint-disable linebreak-style */
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
